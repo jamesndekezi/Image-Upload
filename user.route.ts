7ffe80f338e808b7c1d9decc8f50f7ac23835766
jamesndekezi <111005167+jamesndekezi@users.noreply.github.com>
2024-06-16 21:52:30 +0200

    Create user.route.ts

---
+++ user.route.ts
@@ -0,0 +1,53 @@
+import express from "express";
+import { protectRoute, restrictTo } from "../middlewares/auth.middleware";
+import { verifyEmail } from "../controllers/verifyUser.controller";
+import { verifyTokenMiddleware } from "../middlewares/verifyToken.middleware";
+import {
+  updateRole,
+  userSignup,
+  userLogin,
+  userLogout,
+  changeAccountStatus,
+  updatePassword,
+  requestPasswordReset,
+  resetPassword,
+} from "../controllers/user.controller";
+import {
+  validateUser,
+  validateUserLogin,
+  validateUserUpdatePassword,
+} from "../validations/user.validate";
+import { userRole } from "../utils/variable.utils";
+import { updateUserProfile } from "../controllers/profileController";
+import { upload } from "../utils/multer.utils";
+
+const userRoutes = express.Router();
+
+userRoutes.post("/signup", validateUser, userSignup);
+userRoutes.patch(
+  "/:id/role",
+  protectRoute,
+  restrictTo(userRole.admin),
+  updateRole
+);
+userRoutes.patch(
+  "/change-account-status/:id",
+  protectRoute,
+  restrictTo(userRole.admin),
+  changeAccountStatus
+);
+
+userRoutes.post("/login", validateUserLogin, userLogin);
+userRoutes.post("/logout", protectRoute, userLogout);
+userRoutes.patch(
+  "/:id/updatepassword",
+  protectRoute,
+  validateUserUpdatePassword,
+  updatePassword
+);
+userRoutes.post("/requestpasswordreset", requestPasswordReset);
+userRoutes.post("/resetpassword", resetPassword);
+userRoutes.get("/verify-email", verifyTokenMiddleware, verifyEmail);
+userRoutes.patch("/update-profile", protectRoute, upload, updateUserProfile);
+// userRoutes.get('/update-profile',verifyTokenMiddleware, updateUserProfile);
+export default userRoutes;
